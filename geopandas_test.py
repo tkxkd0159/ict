6c1d2af5c455dea53aefe262111632ae2f76301d
Jaeseung Lee(Home) <41176085+tkxkd0159@users.noreply.github.com>
2021-03-23 22:10:32 +0900

    Test data preprocessing
    With geopandas

--- geopandas_test.py
+++ geopandas_test.py
@@ -1,15 +1,23 @@
 import matplotlib.pyplot as plt
 import geopandas
 
+
 filename = 'custom.geo.json'
 with open(filename) as f:
     df = geopandas.read_file(f)
     print(df.head())
-    df = df.rename(columns={'geometry': 'borders'})
     print(df.shape)
+
     df.insert(1, "test", "default")
+    # df = df.rename(columns={'test': 'new_test'})
+
     df['test'][1] = "change"
     print(df.head())
 
+    # df.to_file('.\\my.json', driver="GeoJSON")
     # df.plot()
     # plt.show()
+
+with open('my.json') as f2:
+    df2 = geopandas.read_file('my.json')
+    print(df2["iso_a3"])
